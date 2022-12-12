24122de16ca1ef4884314816f8a767b16b90a5b4
DBHRN <dbhrnf@gmail.com>
2022-12-12 12:35:25 -0600

    añadiendo node

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+import { create, router as _router, defaults } from 'json-server'
+const server = create()
+const router = _router('db.json')
+const middlewares = defaults()
  
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
-
-export const serverurls = {
-  server,
-  router,
-}
