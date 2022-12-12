4a7c2e88db3cc1009d62018561a9fdb6a2a92cbb
DBHRN <dbhrnf@gmail.com>
2022-12-12 12:03:48 -0600

    añadiendo arreglos

--- servicios/producto-servicios.js
+++ servicios/producto-servicios.js
@@ -1,18 +1,18 @@
 //GET
 const listaProductos = () =>
-  fetch("router")
+  fetch(server + "/producto")
     .then((resposta) => resposta.json())
     .catch((error) => console.log(error));
 
 const listarUnProduto = (id) => {
-  return fetch(`router/${id}`).then((resposta) => {
+  return fetch(server + `/${id}`).then((resposta) => {
     return resposta.json();
   });
 };
 
 //POST
 const creaProdutos = (name, imageUrl, price, description, tipo) => {
-  return fetch(`http://localhost:3000/producto`, {
+  return fetch(server + `/producto`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -35,7 +35,7 @@ const creaProdutos = (name, imageUrl, price, description, tipo) => {
 
 // PUT/PATCH
 const alteraProduto = async (id, imageUrl, name, price, description) => {
-  return fetch(`http://localhost:3000/producto/${id}`, {
+  return fetch(server + `/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
@@ -55,7 +55,7 @@ const alteraProduto = async (id, imageUrl, name, price, description) => {
 
 // DELETE
 const deleteProducto = async (id) => {
-  return await fetch(`http://localhost:3000/producto/${id}`, {
+  return await fetch(server + `/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -1,4 +1,4 @@
-const listaProductos = () => fetch(" router").then(respuesta => respuesta.json());
+const listaProductos = () => fetch(server + "/producto").then(respuesta => respuesta.json());
 
 
 
