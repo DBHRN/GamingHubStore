164c710d98fa270e6414a46f627b1d11ae36edff
DBHRN <dbhrnf@gmail.com>
2022-12-12 13:28:42 -0600

    añadiendo arreglos

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // server.js
-import { create, router as _router, defaults } from 'json-server'
-const server = create()
-const router = _router('db.json')
-const middlewares = defaults()
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use('', router)
